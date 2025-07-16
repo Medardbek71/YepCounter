a817d12d66998369f960d7c8dd6d0828e593dc6c
Medard <medardbek71@gmail.com>
2025-07-16 13:31:25 +0100

    initialisation ts

---
+++ screens/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View>
+      <Text>Le tampico</Text>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
